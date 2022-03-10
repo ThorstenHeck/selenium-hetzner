6c0d545ca62e248e4e80778fbdf458646ac492fd
thorstenheck <simonparmesan@outlook.de>
2022-03-10 19:40:13 +0100

    enable headless mode again

--- hetzner_login/hetzner_login.py
+++ hetzner_login/hetzner_login.py
@@ -73,7 +73,7 @@ permissions = permissions or "Read & Write"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--window-size=1920,1080")
